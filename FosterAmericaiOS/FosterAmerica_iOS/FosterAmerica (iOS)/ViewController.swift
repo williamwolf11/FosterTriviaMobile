d0a07acf702f99c230344923b25b1a84f1bab8d8
Paul Farcasanu <pfarcasanu@gmail.com>
2018-09-16 23:12:57 -0500

    Cookie changes -- not functional

--- FosterAmericaiOS/FosterAmerica_iOS/FosterAmerica (iOS)/ViewController.swift
+++ FosterAmericaiOS/FosterAmerica_iOS/FosterAmerica (iOS)/ViewController.swift
@@ -8,22 +8,57 @@
 
 import UIKit
 import WebKit
-
-class ViewController: UIViewController {
-
-    @IBOutlet weak var appWebView: WKWebView!
+class ViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let app_url = URL(string: "https://pfarcasanu.github.io/FosterTrivia/");
-        appWebView.load(URLRequest(url: app_url!));
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(appMovedToBackground), name: Notification.Name.UIApplicationWillResignActive, object: nil)
+        
+        let myURL = URL(string:"https://www.181seconds.com/FosterAmerica")
+        let myRequest = URLRequest(url: myURL!)
+        
+        loadCookie()
+        
+        webView.load(myRequest)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func appMovedToBackground() {
+        saveCookie()
     }
-
-
+    
+    func saveCookie() {
+        let cookieJar: HTTPCookieStorage = HTTPCookieStorage.shared
+        if let cookies = cookieJar.cookies {
+            let data: Data = NSKeyedArchiver.archivedData(withRootObject: cookies)
+            let ud: UserDefaults = UserDefaults.standard
+            ud.set(data, forKey: "cookie")
+        }
+    }
+    
+    func loadCookie() {
+        let ud: UserDefaults = UserDefaults.standard
+        let data: Data? = ud.object(forKey: "cookie") as? Data
+        if let cookie = data {
+            let datas: NSArray? = NSKeyedUnarchiver.unarchiveObject(with: cookie) as? NSArray
+            if let cookies = datas {
+                for c in cookies {
+                    if let cookieObject = c as? HTTPCookie {
+                        HTTPCookieStorage.shared.setCookie(cookieObject)
+                    }
+                }
+            }
+        }
+    }
+    
 }
-
